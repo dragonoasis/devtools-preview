a38824e1a89797752a74b88cd245c24d8ee742cd
dragonoasis <dragonoasis@users.noreply.github.com>
2026-03-30 03:58:11 +0000

    deploy: dragonoasis/devtools-hub@333e8d0ed1237c9f1cec1d5a233a2d370f3bc3e5

--- _nuxt/PuLa7Wa0.js
+++
@@ -1,1 +0,0 @@
-import{u as l}from"./D8gERitg.js";import{y as s}from"./B7T3FRA7.js";function f(){const{copy:u,isSupported:t}=l(),e=s(!1);let r=null;async function a(o){if(!t.value)return!1;try{return await u(o),e.value=!0,r&&clearTimeout(r),r=setTimeout(()=>{e.value=!1},2e3),!0}catch{return e.value=!1,!1}}return{copy:a,copied:e,isSupported:t}}export{f as u};
